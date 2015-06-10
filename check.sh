110ef04475d1f681d9139ce0d0c0b20bbbdf01d2
b4b4r07 <b4b4r07@gmail.com>
2015-06-10 21:56:27 +0900

    Add image

--- check.sh
+++ check.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-text="$(ruby ./nokogiri.rb 156 165 | nkf)"
+text="$(ruby ./nokogiri.rb 156 165 2>/dev/null | nkf)"
 if [ -n "$text" ]; then
-    terminal-notifier -title "美原→未来大学" -message "$text" -open "http://www.hakobus.jp/result.php?in=156&out=165" -contentImage "/Users/b4b4r07/Desktop/bus.png" -sound Submarine
+    terminal-notifier -title "美原→未来大学" -message "$text" -open "http://www.hakobus.jp/result.php?in=156&out=165" -contentImage "bus.png" -sound Submarine
 fi
 
 
-text="$(ruby ./nokogiri.rb 165 156 | nkf)"
+text="$(ruby ./nokogiri.rb 165 156 2>/dev/null | nkf)"
 if [ -n "$text" ]; then
-    terminal-notifier -title "未来大学→美原" -message "$text" -open "http://www.hakobus.jp/result.php?in=156&out=165" -contentImage "/Users/b4b4r07/Desktop/bus.png" -sound Submarine
+    terminal-notifier -title "未来大学→美原" -message "$text" -open "http://www.hakobus.jp/result.php?in=156&out=165" -contentImage "bus.png" -sound Submarine
 fi
